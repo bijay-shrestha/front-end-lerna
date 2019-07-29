602a16d4f6ca0dfeaf820ceb9e5425e8078f7d38
Bijay Shrestha <bijay.shrestha@f1soft.com>
2019-07-29 17:21:35 +0545

    Refactor root package.json

---
+++ apps/ui-elements/CInputGroup/lib/CInputGroup.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+const CInputGroup = styled.input`
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    font-size: 13px;
+    font-weight: 300;
+    padding: 10px 40px 10px 10px;
+    width: 250px;
+`;
+export default CInputGroup;
