8352cffcb1d1b59caad12962e243e76481ffb591
sabu.shakya <sabu.shakya@f1soft.com>
2019-08-01 14:46:28 +0545

    Add breadcrumb data in story

--- apps/ui-components/CBreadcrumb/CBreadcrum.story.js
+++ apps/ui-components/CBreadcrumb/CBreadcrum.story.js
@@ -6,9 +6,31 @@ import {HashRouter} from 'react-router-dom';
 
 storiesOf('CBreadcrumb', module)
     .add('default CBreadcrumb', () => {
+        const dataForBreadCrumb = [
+            {
+                id: '1',
+                name: 'Home',
+                path: '/home'
+            },
+            {
+                id: '2',
+                name: 'General Setup',
+                path: '/generalSetup'
+            },
+            {
+                id: '3',
+                name: 'Admin Setup',
+                path: '/adminSetup'
+            },
+            {
+                id: '4',
+                name: 'Add Admin',
+                path: '/adminSetup/add'
+            },
+        ];
         return (
             <HashRouter>
-                <CBreadcrumb/>
+                <CBreadcrumb breadcrumbData={dataForBreadCrumb}/>
             </HashRouter>
         )
     });
