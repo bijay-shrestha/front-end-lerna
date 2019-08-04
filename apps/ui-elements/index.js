e613ffff799f69334ed61c10270210959ec995be
Bijay Shrestha <bijay.shrestha@f1soft.com>
2019-08-04 17:09:54 +0545

    Minor fix.

--- apps/ui-elements/index.js
+++ apps/ui-elements/index.js
@@ -1,7 +1,3 @@
-// export {default as CButton} from './src/CButton';
-// export {default as CDropdown} from './src/CDropdown';
-// export {default as CInputGroup} from './src/CInputGroup';
-
 import CButton from './src/CButton';
 import CDropdown from './src/CDropdown';
 import CInputGroup from './src/CInputGroup';
